f8af12cc92ea996b0890f394fd38d5c7e460f98f
OsobistSCP <osobist@scpfoundation.net>
2025-05-03 18:22:08 +0700

    <RESTORE>Additional randomType variants (from 10 to 125%) & (from 25 to 125%)

--- src/Ufopaedia/StatsForNerdsState.cpp
+++ src/Ufopaedia/StatsForNerdsState.cpp
@@ -1298,6 +1298,8 @@ void StatsForNerdsState::addDamageRandomType(std::ostringstream &ss, const ItemD
 		case DRT_EASY: ss << tr("DRT_EASY"); break;
 		case DRT_STANDARD: ss << tr("DRT_STANDARD"); break;
 		case DRT_EXPLOSION: ss << tr("DRT_EXPLOSION"); break;
+		case DRT_SCP: ss << tr("DRT_SCP"); break;
+		case DRT_SCP_LIGHT:	ss << tr("DRT_SCP_LIGHT"); break;
 		default: ss << tr("STR_UNKNOWN"); break;
 	}
 	if (_showIds)
