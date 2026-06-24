0dc284a721448a60735805fcddeaab849c73a424
OsobistSCP <osobist@scpfoundation.net>
2026-06-24 13:26:55 +0700

    Copypaste goof made when adding additionl damageType modifiers

--- src/Mod/RuleItem.cpp
+++ src/Mod/RuleItem.cpp
@@ -2940,18 +2940,18 @@ void RuleItem::ScriptRegister(ScriptParserBase* parser)
 
 		rs.add<&getDamageToScript<&RuleDamageType::ToArmorPre>>("getDamageToArmorPre", "calculated damage value multiplied by the corresponding modifier");
 		rs.add<&getDamageToScript<&RuleDamageType::ToArmor>>("getDamageToArmor", "calculated damage value multiplied by the corresponding modifier");
-		rs.add<&getDamageToScript<&RuleDamageType::ToArmorPre> >("getDamageToEnergyPre", "calculated damage value multiplied by the corresponding modifier");
+		rs.add<&getDamageToScript<&RuleDamageType::ToEnergyPre> >("getDamageToEnergyPre", "calculated damage value multiplied by the corresponding modifier");
 		rs.add<&getDamageToScript<&RuleDamageType::ToEnergy>>("getDamageToEnergy", "calculated damage value multiplied by the corresponding modifier");
-		rs.add<&getDamageToScript<&RuleDamageType::ToArmorPre> >("getDamageToHealthPre", "calculated damage value multiplied by the corresponding modifier");
+		rs.add<&getDamageToScript<&RuleDamageType::ToHealthPre> >("getDamageToHealthPre", "calculated damage value multiplied by the corresponding modifier");
 		rs.add<&getDamageToScript<&RuleDamageType::ToHealth>>("getDamageToHealth", "calculated damage value multiplied by the corresponding modifier");
 		rs.add<&getDamageToScript<&RuleDamageType::ToItem>>("getDamageToItem", "calculated damage value multiplied by the corresponding modifier");
 		rs.add<&getDamageToScript<&RuleDamageType::ToMana>>("getDamageToMana", "calculated damage value multiplied by the corresponding modifier");
-		rs.add<&getDamageToScript<&RuleDamageType::ToArmorPre> >("getDamageToMoralePre", "calculated damage value multiplied by the corresponding modifier");
+		rs.add<&getDamageToScript<&RuleDamageType::ToMoralePre> >("getDamageToMoralePre", "calculated damage value multiplied by the corresponding modifier");
 		rs.add<&getDamageToScript<&RuleDamageType::ToMorale>>("getDamageToMorale", "calculated damage value multiplied by the corresponding modifier");
-		rs.add<&getDamageToScript<&RuleDamageType::ToArmorPre> >("getDamageToStunPre", "calculated damage value multiplied by the corresponding modifier");
+		rs.add<&getDamageToScript<&RuleDamageType::ToStunPre> >("getDamageToStunPre", "calculated damage value multiplied by the corresponding modifier");
 		rs.add<&getDamageToScript<&RuleDamageType::ToStun>>("getDamageToStun", "calculated damage value multiplied by the corresponding modifier");
 		rs.add<&getDamageToScript<&RuleDamageType::ToTile>>("getDamageToTile", "calculated damage value multiplied by the corresponding modifier");
-		rs.add<&getDamageToScript<&RuleDamageType::ToArmorPre> >("getDamageToTimePre", "calculated damage value multiplied by the corresponding modifier");
+		rs.add<&getDamageToScript<&RuleDamageType::ToTimePre> >("getDamageToTimePre", "calculated damage value multiplied by the corresponding modifier");
 		rs.add<&getDamageToScript<&RuleDamageType::ToTime>>("getDamageToTime", "calculated damage value multiplied by the corresponding modifier");
 		rs.add<&getDamageToScript<&RuleDamageType::ToWound>>("getDamageToWound", "calculated damage value multiplied by the corresponding modifier");
 
